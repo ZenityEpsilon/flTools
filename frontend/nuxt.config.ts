afc22ed5e291e4cfd888c096198238b97f425cb3
ZenityEpsilon <pica.picali.di@gmail.com>
2025-03-02 20:57:33 +0200

    Add initial api to backend Add tailwind support Add locales to frontend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,18 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  head: {
-    title: 'Freelancer POB',
-    htmlAttrs: {
-      lang: 'ua',
+    head: {
+        title: 'Freelancer POB',
+        htmlAttrs: {
+            lang: 'ua',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@vite-pwa/nuxt',
+        [
+            '@nuxtjs/i18n',
+            {
+                strategy: 'prefix_except_default',
+                defaultLocale: 'ua',
+                langDir: './locales',
+                detectBrowserLanguage: false,
+                redirectOn: false,
+                locales: [
+                    { code: 'ua', iso: 'ua-UA', file: 'ua.js' },
+                    { code: 'en', iso: 'en-EN', file: 'en.js' },
+                ],
+            },
+        ],
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+    css: ['assets/css/fonts.css'],
+    vite: {
+        //plugins: [eslintPlugin()],
+    },
+    nitro: {
+        devProxy: {
+            '/api': { target: process.env.API_URL, changeOrigin: true },
+            '/images': { target: process.env.API_URL + '/images/', changeOrigin: true },
+        },
+    },
+
+    runtimeConfig: {
+        public: {
+            API_BASE_URL: process.env.API_URL,
+        },
+    },
+    devtools: { enabled: true },
+});
